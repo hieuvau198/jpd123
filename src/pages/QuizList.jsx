a6492970fb6a06ba1b2a1e766a33801a1d28fbe9
hieuvau198 <nhatduongfpt2000@gmail.com>
2026-03-02 16:59:14 +0700

    Revert "Update QuizList.jsx"
    This reverts commit a0398785d679e3acbb0fcd9ba1693d27c46e387d.

--- src/pages/QuizList.jsx
+++ src/pages/QuizList.jsx
@@ -48,7 +48,7 @@ const QuizList = () => {
             </button>
             <h2 className="text-3xl sm:text-4xl font-bold text-white drop-shadow-md flex items-center gap-3">
               <Library className="text-yellow-300" />
-              Quiz Library x
+              Quiz Library
             </h2>
           </div>
         </div>
