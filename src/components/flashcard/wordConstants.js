ad3dfa29e99c029b1689515dc4a8fd9573afa98d
hieuvau198 <nhatduongfpt2000@gmail.com>
2026-09-12 17:28:43 +0700

    Update wordConstants.js

--- src/components/flashcard/wordConstants.js
+++ src/components/flashcard/wordConstants.js
@@ -1,9 +1,9 @@
 // RATING SYSTEM CONSTANTS
 export const ALL_LEVELS = [
-  { min: 0, max: 50, title: 'Fat Cat', img: 'https://i.postimg.cc/zfHKSDK4/fat-cat-1.png', color: '#595959' },
-  { min: 51, max: 80, title: 'Wolfie', img: 'https://i.postimg.cc/MHS7Gkcd/wolf-1.jpg', color: '#3c3f42' },
-  { min: 81, max: 99, title: 'The Great Tiger', img: 'https://i.postimg.cc/hPMLK5KM/fat-tiger-1.png', color: '#fa8c16' },
-  { min: 100, max: 100, title: 'King of The Jungle', img: 'https://i.postimg.cc/zXmzVzGV/dragon-1.jpg', color: '#37eef1' },
+  { min: 0, max: 50, title: 'Danh hiệu Mèo Mập', img: 'https://i.postimg.cc/zfHKSDK4/fat-cat-1.png', color: '#595959' },
+  { min: 51, max: 80, title: 'Danh hiệu Cún Con', img: 'https://i.postimg.cc/MHS7Gkcd/wolf-1.jpg', color: '#3c3f42' },
+  { min: 81, max: 99, title: 'Danh hiệu Hổ Béo', img: 'https://i.postimg.cc/hPMLK5KM/fat-tiger-1.png', color: '#fa8c16' },
+  { min: 100, max: 100, title: 'Danh hiệu Kim Long', img: 'https://i.postimg.cc/zXmzVzGV/dragon-1.jpg', color: '#37eef1' },
 ];
 
 export const getRatingInfo = (score) => {
